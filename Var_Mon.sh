5737775d3e7a3c8a66e343ddc27cec787aab0988
Vijay Samanthapuri <vijaysamanthapuri@gmail.com>
2018-02-14 01:31:04 -0500

    this my latest commit

---
+++ Var_Mon.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+df -hP / | tail -n +2 | awk '{print $5}' | cut -d% -f1 | while read UTIL
+do
+if [ $UTIL -gt 30 ]; then #// Checking if file system utilization is above threshold.
+echo "File System is aboove threshold."
+fi
+done
+#This is end of the file
