9e2c9108cd86ebb74e9c83df9f58a46d96d71434
Vijay Samanthapuri <vijaysamanthapuri@gmail.com>
2018-02-14 01:13:10 -0500

    Add comment at if statement to give user more readbility

--- Var_Mon.sh
+++ Var_Mon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 df -hP / | tail -n +2 | awk '{print $5}' | cut -d% -f1 | while read UTIL
 do
-if [ $UTIL -gt 30 ]; then
+if [ $UTIL -gt 30 ]; then #// Checking if file system utilization is above threshold.
 echo "File System is aboove threshold."
 fi
 done
